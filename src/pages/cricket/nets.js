acf751c396a0e2b741c2b58533e914d125e28ca8
Mark Hughes <mark@cyki.com>
2021-03-28 17:08:41 +0100

    Basic nets bookings calendar integration ...

---
+++ src/pages/cricket/nets.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const NetsPage = () => {
+  return (
+    <Layout>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title is-size-4">Cricket Nets Booking</h1>
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <iframe
+                class="has-ratio is-hidden-mobile"
+                title="Golden Hill Cricket Nets Bookings"
+                src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FLondon&amp;src=Y182cTg0NW8zZnM5Mmw2anNmODgyaWt2cGM1a0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%237986CB"
+                style={{
+                  border: 0,
+                  width: "100%",
+                  height: 600,
+                  frameborder: 0,
+                }}
+              ></iframe>
+              <iframe
+                class="has-ratio is-hidden-desktop"
+                title="Golden Hill Cricket Nets Bookings"
+                src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FLondon&amp;src=Y182cTg0NW8zZnM5Mmw2anNmODgyaWt2cGM1a0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%237986CB"
+                style={{
+                  border: 0,
+                  width: "100%",
+                  height: 600,
+                  frameborder: 0,
+                }}
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default NetsPage;
